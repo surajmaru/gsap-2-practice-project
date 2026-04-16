6bd07c8306d6d06984494b6b09466fd3ffd8bffe
Suraj <suraj123maru@gmail.com>
2026-04-17 02:12:28 +0530

    tailwind and colors and fonts setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <h1 className='text-5xl text-violet-300 font-bold'>Welcome to Awards!!</h1>
+    </main>
+  )
+}
+
+export default App
