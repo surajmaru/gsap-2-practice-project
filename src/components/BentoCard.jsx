014ca4c68dd1969693133b3cc15effb8d8d39c92
Suraj <suraj123maru@gmail.com>
2026-04-18 16:08:16 +0530

    features page completed

---
+++ src/components/BentoCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function BentoCard({src, title, description, isComingSoon}) {
+  return (
+    <div className='relative size-full'>
+        <video 
+        src={src}
+        loop
+        muted
+        autoPlay
+        className='absolute left-0 top-0 size-full object-center object-cover'
+        />
+
+        <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
+            <div>
+                <h1 className='bento-title special-font'>{title}</h1>
+                {description && (
+                    <p className='mt-3 max-w-64 text-xs md:text-base'>{description}</p>
+                )}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BentoCard
